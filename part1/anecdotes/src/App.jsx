c2090d2d503b18ba6dddceb111fbe01202e2d1d2
Tzvi H <16guitar@gmail.com>
2026-02-26 14:54:24 +0700

    1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,11 +13,22 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
   return (
     <div>
       <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
       <div>
+        <button
+          onClick={() => {
+            const newVotes = [...votes];
+            newVotes[selected] += 1;
+            setVotes(newVotes);
+          }}
+        >
+          vote
+        </button>
         <button
           onClick={() =>
             setSelected(Math.floor(Math.random() * anecdotes.length))
