bae450134c892d826337f19ac88c56ff90bfa076
Tzvi H <16guitar@gmail.com>
2026-02-27 17:12:32 +0700

    2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+// Header, Content, and Total. All data still resides in the App component, which passes the necessary data to each component
+// using props. Header takes care of rendering the name of the course, Content renders the parts and their number of
+// exercises and Total renders the total number of exercises.
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part, index) => (
+      <Part part={part} key={index} />
+    ))}
+  </div>
+);
+
+const Total = ({ parts }) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return <p>Number of exercises {total}</p>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      {/* <Total parts={course.parts} /> */}
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+      {
+        name: "Typescript",
+        exercises: 25,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
