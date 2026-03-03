e9e94cc65cb78bb415b2fed4effbbdbd68173164
Tzvi H <16guitar@gmail.com>
2026-03-03 18:05:24 +0700

    3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,10 @@
 const morgan = require("morgan");
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token("body", function (req) {
