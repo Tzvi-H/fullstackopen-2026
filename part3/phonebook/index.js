f2f9b41f5857fb75401f5c11d504f6a4badeb8b0
Tzvi H <16guitar@gmail.com>
2026-03-04 16:26:28 +0700

    3.13

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const morgan = require("morgan");
 const express = require("express");
+const Person = require("./models/person");
 
 const app = express();
 
@@ -17,29 +20,6 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/info", (req, res) => {
   res.send(`
         Phonebook has info for ${persons.length} people <br />
@@ -47,7 +27,7 @@ app.get("/info", (req, res) => {
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((result) => res.json(result));
 });
 
 app.get("/api/persons/:id", (req, res) => {
