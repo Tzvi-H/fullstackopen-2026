f0d5a58df5f3c952e3c861bbca5dc6ddb5fa53af
Tzvi H <16guitar@gmail.com>
2026-03-06 18:05:14 +0700

    4.4

--- part4/blogs/tests/blogs.test.js
+++ part4/blogs/tests/blogs.test.js
@@ -1,8 +1,26 @@
 const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
+const blogs = require("./blogs_data");
 
 test("dummy returns 1", () => {
   const result = listHelper.dummy([]);
   assert.strictEqual(result, 1);
+});
+
+describe("total likes", () => {
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has only one blog equals the likes of that", () => {
+    const result = listHelper.totalLikes([blogs[0]]);
+    assert.strictEqual(result, 7);
+  });
+
+  test("of a bigger list, is calculated right", () => {
+    const result = listHelper.totalLikes(blogs);
+    assert.strictEqual(result, 36);
+  });
 });
