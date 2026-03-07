e0bff7d6dd68d82f4740b7b66c6e73bf1a1d4b4c
Tzvi H <16guitar@gmail.com>
2026-03-07 18:19:02 +0700

    4.9

--- part4/blogs/tests/blogs.test.js
+++ part4/blogs/tests/blogs.test.js
@@ -3,14 +3,14 @@ const assert = require("node:assert");
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 const app = require("../app");
-const blogs = require("./blogs_data");
+const initialBlogs = require("./blogs_data");
 const Blog = require("../models/blog");
 
 const api = supertest(app);
 
 beforeEach(async () => {
   await Blog.deleteMany();
-  await Blog.insertMany(blogs);
+  await Blog.insertMany(initialBlogs);
 });
 
 describe("GET /api/blogs", () => {
@@ -20,7 +20,14 @@ describe("GET /api/blogs", () => {
       .expect(200)
       .expect("Content-Type", /application\/json/);
 
-    assert.strictEqual(result.body.length, blogs.length);
+    assert.strictEqual(result.body.length, initialBlogs.length);
+  });
+
+  test("returns blogs that contain an id property, not _id", async () => {
+    const result = await api.get("/api/blogs");
+
+    assert("id" in result.body[0]);
+    assert(!("_id" in result.body[0]));
   });
 });
 
