71553c0afb1666c7e845c624bd7cdcc031e3222a
Tzvi H <16guitar@gmail.com>
2026-03-19 17:34:09 +0700

    5.14

--- part4/bloglist-frontend/src/components/Blog.test.jsx
+++ part4/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,17 +1,39 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
-test("renders content", () => {
-  const blog = {
-    title: "test title",
-    author: "test author",
-    url: "test url",
-    likes: 0,
-    user: null,
-  };
+describe("Blog", () => {
+  beforeEach(() => {
+    const blog = {
+      title: "test title",
+      author: "test author",
+      url: "test url",
+      likes: 0,
+      user: { name: "john doe", username: "john90best" },
+    };
 
-  render(<Blog blog={blog} />);
+    render(<Blog blog={blog} />);
+  });
 
-  const element = screen.getByText("test title", { exact: false });
-  expect(element).toBeDefined();
+  test("renders content", () => {
+    const element = screen.getByText("test title", { exact: false });
+    expect(element).toBeDefined();
+  });
+
+  test("at start the url and likes are not displayed", () => {
+    const element = screen.queryByText("test url", { exact: false });
+    expect(element).not.toBeInTheDocument();
+    const urlElement = screen.queryByText("likes", { exact: false });
+    expect(urlElement).not.toBeInTheDocument();
+  });
+
+  test("shows details after clicking the view button", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText("view");
+    await user.click(button);
+    const element = screen.getByText("test url", { exact: false });
+    expect(element).toBeInTheDocument();
+    const urlElement = screen.queryByText("likes", { exact: false });
+    expect(urlElement).toBeInTheDocument();
+  });
 });
