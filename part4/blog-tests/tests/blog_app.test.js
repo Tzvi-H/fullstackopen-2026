b1f2eb769ea16e3b13c8c98731535384e69bbbe9
Tzvi H <16guitar@gmail.com>
2026-03-20 17:58:51 +0700

    5.19

--- part4/blog-tests/tests/blog_app.test.js
+++ part4/blog-tests/tests/blog_app.test.js
@@ -33,11 +33,32 @@ describe("Blog app", () => {
       await page.getByLabel("password").fill("wrong password");
       await page.getByRole("button", { name: "login" }).click();
 
-      //   await expect(page.getByText("wrong username or password")).toBeVisible();
       const errorDiv = page.locator(".error");
       await expect(errorDiv).toContainText("wrong username or password");
       await expect(errorDiv).toHaveCSS("border-style", "solid");
       await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
+    });
+  });
+
+  describe("when logged in", () => {
+    beforeEach(async ({ page }) => {
+      await page.getByLabel("username").fill("username 3");
+      await page.getByLabel("password").fill("123456");
+      await page.getByRole("button", { name: "login" }).click();
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      await page.getByRole("button", { name: "create new blog" }).click();
+      const textboxes = await page.getByRole("textbox").all();
+      await textboxes[0].fill("a blog title created by playwright");
+      await textboxes[1].fill("a blog author created by playwright");
+      await textboxes[2].fill("a blog url created by playwright");
+      await page.getByRole("button", { name: "create" }).click();
+      await expect(
+        page.getByText(
+          "a blog title created by playwright a blog author created by playwright",
+        ),
+      ).toBeVisible();
     });
   });
 });
