5d917302395314932f8c7a720f9b9d29eb553def
Tzvi H <16guitar@gmail.com>
2026-03-20 17:45:12 +0700

    5.18

--- part4/blog-tests/tests/blog_app.test.js
+++ part4/blog-tests/tests/blog_app.test.js
@@ -1,12 +1,35 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("http://localhost:3003/api/testing/reset");
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        name: "name 3",
+        password: "123456",
+        username: "username 3",
+      },
+    });
+
     await page.goto("http://localhost:5173");
   });
 
   test("Login form is shown", async ({ page }) => {
     const locator = page.getByText("log in to application");
     await expect(locator).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByLabel("username").fill("username 3");
+      await page.getByLabel("password").fill("123456");
+      await page.getByRole("button", { name: "login" }).click();
+
+      await expect(page.getByText("name 3 logged in")).toBeVisible();
+    });
+
+    // test('fails with wrong credentials', async ({ page }) => {
+    //   // ...
+    // })
   });
 });
