504a6c0c9dce00a9bc4d3efa0e16253ac8d8bf76
Tzvi H <16guitar@gmail.com>
2026-03-25 18:50:56 +0700

    6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -4,16 +4,21 @@ import { voteFor } from "../reducers/anecdoteReducer";
 const AnecdoteList = () => {
   const dispatch = useDispatch();
   const anecdotes = useSelector((state) =>
-    [...state].sort((a, b) => b.votes - a.votes),
+    [...state.anecdotes].sort((a, b) => b.votes - a.votes),
   );
+  const filter = useSelector((state) => state.filter);
 
   const vote = (id) => {
     dispatch(voteFor(id));
   };
 
+  const filteredAnecdotes = anecdotes.filter((a) =>
+    a.content.toLowerCase().includes(filter.toLowerCase()),
+  );
+
   return (
     <div>
-      {anecdotes.map((anecdote) => (
+      {filteredAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
