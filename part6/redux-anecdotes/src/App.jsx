b4934d7c9e7969fd5ebf41287be2d962c656e2a1
Tzvi H <16guitar@gmail.com>
2026-03-27 18:09:37 +0700

    6.14

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import anecdotesService from "./services/anecdotes";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
 import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdotesService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  }, [dispatch]);
+
   return (
     <div>
       <Notification />
